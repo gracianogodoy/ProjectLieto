ef6acb58a83ac4b64de9fbb09f6e58680949cd60
gracianogodoy <gracianogodoy@gmail.com>
2016-11-17 00:33:04 -0200

    - Cleanup

--- Assets/Scripts/Characters/Boss/BossViewController.cs
+++ Assets/Scripts/Characters/Boss/BossViewController.cs
@@ -2,26 +2,26 @@
 
 public class BossViewController : MonoBehaviour
 {
-    private Life _life;
-    private Animator _animator;
+    //private Life _life;
+    //private Animator _animator;
 
-    void Start()
-    {
-        _life = GetComponentInParent<Life>();
-        _animator = GetComponent<Animator>();
+    //void Start()
+    //{
+    //    _life = GetComponentInParent<Life>();
+    //    _animator = GetComponent<Animator>();
 
-        _life.OnDead += onDead;
-        _life.OnTakeDamage += onTakeDamage;
-    }
+    //    _life.OnDead += onDead;
+    //    _life.OnTakeDamage += onTakeDamage;
+    //}
 
-    private void onTakeDamage(int damage)
-    {
-        if (_life.CurrentLife > 0)
-            _animator.SetTrigger("Hit");
-    }
+    //private void onTakeDamage(int damage)
+    //{
+    //    if (_life.CurrentLife > 0)
+    //        _animator.SetTrigger("Hit");
+    //}
 
-    private void onDead()
-    {
-        _animator.SetTrigger("Dead");
-    }
+    //private void onDead()
+    //{
+    //    _animator.SetTrigger("Dead");
+    //}
 }
