5273ecbfd86c74b3514eeacf82fcfdef1a24678c
gracianogodoy <gracianogodoy@gmail.com>
2016-11-18 18:06:00 -0200

    -

--- Assets/Scripts/General/StoryFlow.cs
+++ Assets/Scripts/General/StoryFlow.cs
@@ -11,6 +11,7 @@ namespace GG
         private RPGTalk _talk;
         private InputHandler _input;
         private Settings _settings;
+        private int _currentAct;
 
         public StoryFlow(TalkCallback talkCallback, InputHandler input, Settings settings, RPGTalk talk)
         {
@@ -23,9 +24,17 @@ namespace GG
         public void Initialize()
         {
             _talkCallback.OnTalkFinish += onTalkFinish;
+            _currentAct = -1;
             _input.SetEnable(false);
 
-            Timing.RunCoroutine(flow());
+            Timing.RunCoroutine(startStory());
+        }
+
+        public void NextAct()
+        {
+            _currentAct++;
+            _input.SetEnable(false);
+            startAct(_settings.acts[_currentAct]);
         }
 
         private void onTalkFinish()
@@ -33,18 +42,35 @@ namespace GG
             _input.SetEnable(true);
         }
 
-        private IEnumerator<float> flow()
+        private IEnumerator<float> startStory()
         {
             yield return Timing.WaitForSeconds(_settings.timeToStart);
 
-            _talk.NewTalk();
+            NextAct();
+        }
 
+        private void startAct(Settings.Act act)
+        {
+            _talk.txtToParse = act.dialogueText;
+            _talk.lineToStart = act.startLine;
+            _talk.lineToBreak = act.endLine;
+
+            _talk.NewTalk();
         }
 
         [System.Serializable]
         public class Settings
         {
             public float timeToStart;
+            public Act[] acts;
+
+            [System.Serializable]
+            public class Act
+            {
+                public TextAsset dialogueText;
+                public int startLine;
+                public int endLine;
+            }
         }
     }
 }
